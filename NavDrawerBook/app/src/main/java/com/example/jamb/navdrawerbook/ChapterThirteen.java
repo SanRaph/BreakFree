78f91bedb96f72472416e8bedf661100ca125f24
SanRaph <raphytex@gmail.com>
2018-06-08 06:47:03 +0800

    final book

---
+++ NavDrawerBook/app/src/main/java/com/example/jamb/navdrawerbook/ChapterThirteen.java
@@ -0,0 +1,31 @@
+package com.example.jamb.navdrawerbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ViewFlipper;
+public class ChapterThirteen extends AppCompatActivity implements View.OnClickListener {
+    ViewFlipper viewFlipper;
+    Button next;
+    Button previous;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.thirteen);
+        viewFlipper = findViewById(R.id.viewFlipper);
+        next = findViewById(R.id.next);
+        previous = findViewById(R.id.previous);
+        next.setOnClickListener(this);
+        previous.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        if (v == next) {
+            viewFlipper.showNext();
+        }
+        else if (v == previous) {
+            viewFlipper.showPrevious();
+        }
+    }
+}
